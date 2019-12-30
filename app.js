e5b7dfd8d97c12737c47a29bdc646492f6a782ab
xatier <xatierlike@gmail.com>
2019-12-29 20:24:19 -0800

    Update domain name to suisiann.ithuan.tw
    Follow upstream changes
    
    https://github.com/i3thuan5/tau3_phah4-ji7/pull/30/files
    https://github.com/SuiSiann/Sui-Siann-BangTsam/pull/10/files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ chrome.contextMenus.create({
     "title": "Liām hōo lín-pē thiann \"%s\" on 鬥拍字.意傳.台灣",
     "contexts": ['selection'],
     "onclick": function (info, tab) {
-        var url = 'https://鬥拍字.意傳.台灣/講/'
+        var url = 'https://suisiann.ithuan.tw/講/'
             + encodeURIComponent(info.selectionText)
         chrome.tabs.create({ url : url });
     }
