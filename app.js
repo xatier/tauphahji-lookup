f6deefa805687481e93ad04e78edccec46b803a6
xatier <xatierlike@gmail.com>
2022-08-13 22:37:46 -0500

    feat: voice selection by name

--- app.js
+++ app.js
@@ -10,24 +10,44 @@ chrome.contextMenus.create({
     },
 })
 
+// https://stackoverflow.com/a/59786665
+const allVoicesObtained = new Promise(function (resolve, reject) {
+    let voices = speechSynthesis.getVoices()
+    if (voices.length !== 0) {
+        resolve(voices)
+    } else {
+        speechSynthesis.addEventListener('voiceschanged', function () {
+            voices = speechSynthesis.getVoices()
+            resolve(voices)
+        })
+    }
+})
+
 // https://twitter.com/bcrypt/status/1500348547887079424
-const speakIn = (text, lang) => {
+const speakIn = async (text, lang, name) => {
+    const voices = await allVoicesObtained
     const u = new SpeechSynthesisUtterance(text)
+
+    // to pick a new voice
+    // console.log('available voices')
+    // console.log(voices.filter((v) => v.lang === lang))
+
     u.lang = lang
+    u.voice = voices.filter((v) => v.lang === lang && v.name.includes(name))[0]
     speechSynthesis.speak(u)
 }
 
 ;[
-    ['English', 'en-US'],
-    ['日本語', 'ja-JP'],
-    ['中文', 'zh-TW'],
-    ['廣東話', 'zh-HK'],
-].forEach(([lang, code]) => {
+    ['English', 'en-US', 'Microsoft Aria Online'],
+    ['日本語', 'ja-JP', 'Microsoft Nanami Online'],
+    ['中文', 'zh-TW', 'Microsoft HsiaoChen Online'],
+    ['廣東話', 'zh-HK', 'Microsoft HiuGaai Online'],
+].forEach(([lang, code, name]) => {
     chrome.contextMenus.create({
         title: `Speak in ${lang}`,
         contexts: ['selection'],
-        onclick: function (info, tab) {
-            speakIn(info.selectionText, code)
+        onclick: async function (info, tab) {
+            speakIn(info.selectionText, code, name)
         },
     })
 })
